8aeb20d9c313beba6cd9c383f33ebd6f360b0b28
Tobias Oitzinger <tobiasoitzinger@gmail.com>
2019-02-21 17:32:09 +0100

    make ul and li expandable

---
+++ assets/js/documenation.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    ul_expandable();
+});
+
+function ul_expandable() {
+    let parent_ul = $('.doc-nav ul').first();
+    $(parent_ul).find('> li').each(function() {
+        if ($(this).has('> ul').length) {
+            $(this).addClass('expandable');
+            $(this).find('ul li').has('ul').each(function () {
+               $(this).addClass('expandable');
+            });
+        }
+        if ($(this).has('> a').length === 0) {
+            $(this).addClass('cursor-default');
+        }
+    });
+}
+
+
+$('ul li').click(function (e) {
+    e.stopPropagation();
+    if ($(this).hasClass('expandable')) {
+        $(this).find('> ul').slideToggle();
+    }
+});
